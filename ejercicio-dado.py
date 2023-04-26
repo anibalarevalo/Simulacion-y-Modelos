c0fc495c5d186a4607cf9176aa6a515abe1c268f
anibalarevalo <79030561+anibalarevalo@users.noreply.github.com>
2023-04-26 09:57:41 -0400

    Ejercicio de lanzamiento de dados

---
+++ ejercicio-dado.py
@@ -0,0 +1,30 @@
+import random
+
+def lanzamiento_dado():
+    dinero = 100
+    dinero_maximo = 0
+    movimiento = 0
+
+    while dinero != 0:
+        dado = random.randint(1,6)
+        
+        if dado % 2 == 0:
+            dinero += 100
+        else:
+            dinero -= 100
+        
+        if dinero > dinero_maximo:
+            dinero_maximo = dinero
+
+        movimiento += 1
+    
+    mostrar_Resultado(dinero_maximo, movimiento)
+
+def mostrar_Resultado(maximo, movimiento):
+    print(f"Cantidad de movimientos antes de caer en bancarrota: {movimiento}")
+    print(f"Cantidad de dinero máxima alcanzada antes de caer en bancarrota: {maximo}")
+
+def main():
+    lanzamiento_dado()
+
+main()
